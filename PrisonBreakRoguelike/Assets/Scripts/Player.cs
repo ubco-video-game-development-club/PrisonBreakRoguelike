593840599415a5f466a3a06d82e47b223cbdc938
Jaden Balogh <jadenbalogh@gmail.com>
2019-09-01 16:14:55 -0400

    Add core classes
    Includes Player, Tile and Room classes, basic tile instantiation and simple player movement.

---
+++ PrisonBreakRoguelike/Assets/Scripts/Player.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float speed = 1f;
+
+    void Update()
+    {
+        float dx = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
+        float dy = Input.GetAxis("Vertical") * speed * Time.deltaTime;
+        transform.position += new Vector3(dx, dy, 0f);
+    }
+}
---
+++ PrisonBreakRoguelike/Assets/Scripts/Room.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Room : MonoBehaviour
+{
+    public int width = 16, height = 16;
+    public float tileSize = 1f;
+    public Tile tilePrefab;
+
+    private Tile[,] tiles;
+
+    public Tile GetTile(int x, int y)
+    {
+        if (x < 0 || x >= tiles.GetLength(0) ||
+            y < 0 || y >= tiles.GetLength(1))
+        {
+            return null;
+        }
+
+        return tiles[x, y];
+    }
+
+    void Start()
+    {
+        tiles = new Tile[width, height];
+        for (int i = 0; i < width; i++)
+        {
+            for (int j = 0; j < height; j++)
+            {
+                Vector2 pos = new Vector2(i + tileSize/2, j + tileSize/2);
+                Tile tile = Instantiate(tilePrefab, pos, Quaternion.identity) as Tile;
+                tile.x = i;
+                tile.y = j;
+                tiles[i, j] = tile;
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
