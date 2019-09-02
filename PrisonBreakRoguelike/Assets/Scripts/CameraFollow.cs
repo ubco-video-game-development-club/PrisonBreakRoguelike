06b3636c9bf00e79b55b061256cd7cffdf6028e3
Jaden Balogh <jadenbalogh@gmail.com>
2019-09-02 15:04:40 -0400

    Add smooth movement and camera follow

---
+++ PrisonBreakRoguelike/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    [Tooltip("Time it takes to reach the target.")]
+    public float followTime = 0.1f;
+
+    private Transform target;
+    private Vector3 velocity;
+
+    void Start()
+    {
+        target = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+    void Update()
+    {
+        Vector3 targetPosition = new Vector3(target.position.x, target.position.y, transform.position.z);
+        transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref velocity, followTime);
+    }
+}
--- PrisonBreakRoguelike/Assets/Scripts/Player.cs
+++ PrisonBreakRoguelike/Assets/Scripts/Player.cs
@@ -6,10 +6,36 @@ public class Player : MonoBehaviour
 {
     public float speed = 1f;
 
+    private Rigidbody2D rb2d;
+
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
     void Update()
     {
-        float dx = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
-        float dy = Input.GetAxis("Vertical") * speed * Time.deltaTime;
-        transform.position += new Vector3(dx, dy, 0f);
+        float dx = 0f, dy = 0f;
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+        {
+            dx--;
+        }
+        if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+        {
+            dx++;
+        }
+        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
+        {
+            dy++;
+        }
+        if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
+        {
+            dy--;
+        }
+        Vector2 vel = new Vector2(dx, dy);
+        vel.Normalize();
+        vel *= speed;
+        transform.position += new Vector3(vel.x, vel.y, 0f) * Time.deltaTime;
+        //rb2d.velocity = vel;
     }
 }
