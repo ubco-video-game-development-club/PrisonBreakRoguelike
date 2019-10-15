cd1e193e88f203bcd5f3c3100b7ff3605e865d8c
Griffin <griffinbrome@gmail.com>
2019-10-15 15:45:15 -0700

    Removed color field

--- PrisonBreakRoguelike/Assets/Scripts/Stairs.cs
+++ PrisonBreakRoguelike/Assets/Scripts/Stairs.cs
@@ -4,12 +4,6 @@ using UnityEngine;
 
 public class Stairs  : MonoBehaviour 
 {
-    public Color color;    
-    void Start()
-    {
-        GetComponent<SpriteRenderer>().color = color;
-    }
-
     public void  SetAsTarget(bool isTarget)
     {
          GetComponent<LevelController>().NewLevel(); //Untested
