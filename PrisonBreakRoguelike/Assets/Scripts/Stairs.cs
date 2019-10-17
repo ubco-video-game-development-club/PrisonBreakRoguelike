cc55bb9f177d44902ed17f4f9eda1cc6d3f767b5
Griffin <griffinbrome@gmail.com>
2019-10-16 23:31:54 -0700

    made stairs child of item

--- PrisonBreakRoguelike/Assets/Scripts/Stairs.cs
+++ PrisonBreakRoguelike/Assets/Scripts/Stairs.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Stairs  : MonoBehaviour 
+public class Stairs : MonoBehaviour  // Stairs shares functionality w/ other items, but only one is generated per level
 {
     public void  SetAsTarget(bool isTarget)
     {
